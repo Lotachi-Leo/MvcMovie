7466e18788e804810594e88ed804eb70aac77c6b
Lotachi Obi-Nwaigwe <W0873002@myscc.ca>
2025-05-31 11:25:52 -0400

    My first Push

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -10,7 +10,8 @@ public static class SeedData
 {
     public static void Initialize(IServiceProvider serviceProvider)
     {
-        using (var context = new MvcMovieContext(serviceProvider.GetRequiredService<
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
                 DbContextOptions<MvcMovieContext>>()))
         {
             // Look for any movies.
@@ -21,29 +22,76 @@ public static class SeedData
             context.Movie.AddRange(
                 new Movie
                 {
-                    Title = "Rise Up",
-                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Title = "Get Out",
+                    ReleaseDate = DateTime.Parse("2019-2-12"),
                     Genre = "Romantic Comedy",
                     Rating = "R",
                     Price = 7.99M
                 },
                 new Movie
                 {
-                    Title = "Save the Last Dance ",
-                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Title = "Coming to America ",
+                    ReleaseDate = DateTime.Parse("1999-3-13"),
                     Genre = "Comedy",
                     Rating = "R",
                     Price = 8.99M
                 },
                 new Movie
                 {
-                    Title = "ExMachina",
-                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Title = "Pluto",
+                    ReleaseDate = DateTime.Parse("2002-2-23"),
                     Genre = "Comedy",
-                    Rating = "R",
                     Price = 9.99M
-                }
-        );
+                },
+                new Movie
+                {
+                    Title = "Wild Wild West",
+                    ReleaseDate = DateTime.Parse("2009-4-15"),
+                    Genre = "Western",
+                    Rating = "R",
+                    Price = 3.99M
+                },
+                 new Movie
+                 {
+                     Title = "Heaven's Gate",
+                     ReleaseDate = DateTime.Parse("2011-4-15"),
+                     Genre = "Western",
+                     Rating = "R",
+                     Price = 3.99M
+                 }, new Movie
+                 {
+                     Title = "Johnny Bravo",
+                     ReleaseDate = DateTime.Parse("1959-4-15"),
+                     Genre = "Western",
+                     Rating = "R",
+                     Price = 3.99M
+                 },
+                  new Movie
+                  {
+                      Title = "Pretty Woman",
+                      ReleaseDate = DateTime.Parse("2001-4-06"),
+                      Genre = "Western",
+                      Rating = "R",
+                      Price = 3.99M
+                  },
+                   new Movie
+                   {
+                       Title = "Forest Grump",
+                       ReleaseDate = DateTime.Parse("2013-4-17"),
+                       Genre = "Western",
+                       Rating = "R",
+                       Price = 3.99M
+                   },
+                    new Movie
+                    {
+                        Title = "Rico",
+                        ReleaseDate = DateTime.Parse("1991-4-15"),
+                        Genre = "Western",
+                        Rating = "R",
+                        Price = 3.99M
+                    }
+
+            );
             context.SaveChanges();
         }
     }
